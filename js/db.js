288d23531f9658f10b47ac0433b505635716447e
Arpit Mittal <arpit@roofpik.com>
2016-12-09 15:40:08 +0530

    db updated

--- js/db.js
+++ js/db.js
@@ -1,22 +1,22 @@
 //Development DB
 
-// var config = {
-//     apiKey: "AIzaSyAapASzaLGFxHgbMpu9Cibfn97MSEheCcU",
-//     authDomain: "roofpik-f8f55.firebaseapp.com",
-//     databaseURL: "https://roofpik-f8f55.firebaseio.com",
-//     storageBucket: "roofpik-f8f55.appspot.com",
-//     messagingSenderId: "303104341936"
-//   };
+var config = {
+    apiKey: "AIzaSyAapASzaLGFxHgbMpu9Cibfn97MSEheCcU",
+    authDomain: "roofpik-f8f55.firebaseapp.com",
+    databaseURL: "https://roofpik-f8f55.firebaseio.com",
+    storageBucket: "roofpik-f8f55.appspot.com",
+    messagingSenderId: "303104341936"
+  };
 
 
 // Production DB
 
-var config = {
-    apiKey: "AIzaSyD_4bPp5HKryXDzaQwfRhqAU5SsSAdDIJ0",
-    authDomain: "roofpik-948d0.firebaseapp.com",
-    databaseURL: "https://roofpik-948d0.firebaseio.com",
-    storageBucket: "roofpik-948d0.appspot.com",
-};
+// var config = {
+//     apiKey: "AIzaSyD_4bPp5HKryXDzaQwfRhqAU5SsSAdDIJ0",
+//     authDomain: "roofpik-948d0.firebaseapp.com",
+//     databaseURL: "https://roofpik-948d0.firebaseio.com",
+//     storageBucket: "roofpik-948d0.appspot.com",
+// };
 
 
 firebase.initializeApp(config);
