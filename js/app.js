9b1c9a1e167afc4b31f051702150f9af64ded511
Arpit Mittal <arpit@roofpik.com>
2016-12-23 13:59:01 +0530

    login completed

--- js/app.js
+++ js/app.js
@@ -3,44 +3,82 @@ var app = angular.module('roofpik', ['ngMaterial', 'ui.router']);
 app.run(function($rootScope, $mdDialog, $timeout, UserTokenService) {
     $rootScope.loginStatus = false;
     $rootScope.uid = null;
+    $rootScope.localStorageCount = 0;
+    $scope.cityId = '-KYJONgh0P98xoyPPYm9';
+    
     firebase.auth().onAuthStateChanged(function(user) {
-
-        if (user && $rootScope.loginSuccess) {
-            // var storageLoginStatus = localStorage.getItem(loginStatus);
+        if (user && $rootScope.uid == null) {
             $rootScope.uid = user.uid;
-            var timestamp = new Date().getTime();
-            UserTokenService.checkToken($rootScope.uid, timestamp, 4);
-            $rootScope.loginStatus = true;
-
-            localStorage.setItem('loginStatus', true);
-
-            $mdDialog.hide();
-            var name = user.displayName.split(" ");
-            var uname = name[0].charAt(0).toUpperCase() + name[0].slice(1);
-            sweetAlert("Welcome " + uname, "You have successfully logged in!", "success");
-            // User is signed in.
-        } else if ($rootScope.logoutSuccess) {
-            $rootScope.uid = null;
-            $rootScope.loginStatus = false;
-            localStorage.setItem('loginStatus', false);
-            $mdDialog.hide();
-            sweetAlert("Logout Successful", "You have successfully logged out!", "success");
-            // No user is signed in.
-        }
-        else if(user){
             var timestamp = new Date().getTime();
             UserTokenService.checkToken($rootScope.uid, timestamp, 4);
             $rootScope.loginStatus = true;
             localStorage.setItem('loginStatus', true);
         }
     });
+
+    db.ref('dataVersions').once('value', function(response) {
+        $scope.dataVersions = response.val();
+    }).then(function() {
+        $timeout(function() {
+            if (checkLocalStorage('projectList')) {
+                var projectListVersion = (getLocalStorage('projectList')).version;
+                if (projectListVersion != $scope.dataVersions.projectList) {
+                    getProjectList($scope.dataVersions.projectList);
+                }
+                else{
+                    $rootScope.localStorageCount = $rootScope.localStorageCount + 1;
+                }
+            } else {
+                getProjectList($scope.dataVersions.projectList);
+            }
+
+            if (checkLocalStorage('searchList')) {
+                var searchListVersion = (getLocalStorage('searchList')).version;
+                if (searchListVersion != $scope.dataVersions.searchList) {
+                    getSearchList($scope.dataVersions.searchList);
+                }
+                else{
+                    $rootScope.localStorageCount = $rootScope.localStorageCount + 1;
+                }
+            } else {
+                getSearchList($scope.dataVersions.searchList);
+            }
+        }, 100);
+    });
+
+
+    function getProjectList(version) {
+        db.ref('projectList/' + $scope.cityId + '/residential').once('value', function(snapshot) {
+            for (key in snapshot.val()) {
+                $scope.projectList.push(snapshot.val()[key]);
+            }
+            setLocalStorage($scope.projectList, 'projectList', version);
+            $rootScope.localStorageCount = $rootScope.localStorageCount + 1;
+        });
+    };
+
+    function getSearchList(version) {
+        db.ref('search').once('value', function(snapshot) {
+            for (key in snapshot.val()) {
+                $scope.searchList.push(snapshot.val()[key]);
+            }
+            setLocalStorage($scope.searchList, 'searchList', version);
+            $rootScope.localStorageCount = $rootScope.localStorageCount + 1;
+
+        })
+    };
 });
 
-function loading(status) {
-
+function loading(status, timer) {
+    if (!timer) {
+        timer = 10000;
+    }
     if (status) {
-
+        console.log(timer);
         $('.loader-modal').fadeIn();
+        setTimeout(function() {
+            $('.loader-modal').fadeOut();
+        }, timer);
     } else {
         $('.loader-modal').fadeOut();
     }
--- js/controllers/header.ctrl.js
+++ js/controllers/header.ctrl.js
@@ -1,21 +1,18 @@
 app.controller('headerCtrl', function($scope, $mdDialog, $state, $rootScope, $timeout, UserTokenService) {
     $scope.user = false;
     $scope.gotoHome = function() {
-        console.log('called');
         $state.go('home');
     }
 
-
-    $rootScope.$watch('loginStatus', function(){
-         $scope.loginStatus = $rootScope.loginStatus;
+    $rootScope.$watch('loginStatus', function() {
+        $scope.loginStatus = $rootScope.loginStatus;
     });
 
-    $rootScope.$on("callShowLogin", function(){
-        $timeout(function(){
+    $rootScope.$on("callShowLogin", function() {
+        $timeout(function() {
             $scope.showLogin();
-        },0);
+        }, 0);
     });
-   
 
     $scope.showLogin = function(ev) {
         $mdDialog.show({
@@ -34,18 +31,18 @@ app.controller('headerCtrl', function($scope, $mdDialog, $state, $rootScope, $ti
     };
 
     $scope.logout = function() {
-        
+
         var timestamp = new Date().getTime();
         UserTokenService.checkToken($rootScope.uid, timestamp, 5);
-        $rootScope.logoutSuccess = true;
         firebase.auth().signOut().then(function() {
             // Sign-out successful.
-            $timeout(function(){
-                $scope.loginStatus = false;
+            $timeout(function() {
+                $rootScope.uid = null;
                 $rootScope.loginStatus = false;
-                deleteLocalStorage('loginStatus');
-
-            },100);
+                localStorage.setItem('loginStatus', false);
+                $mdDialog.hide();
+                sweetAlert("Logout Successful", "You have successfully logged out!", "success");
+            }, 100);
 
         }, function(error) {
             // An error happened.
@@ -80,6 +77,18 @@ app.controller('headerCtrl', function($scope, $mdDialog, $state, $rootScope, $ti
         $state.go('user-all-reviews');
     };
 
-    loading(false);
+      $scope.goToCoverStories = function() {
+        $state.go('cover-stories', { city: 'gurgaon', cityId: $scope.cityId, from: 1 });
+    }
+
+    $scope.goToBlogs = function() {
+        $state.go('blogs', { city: 'gurgaon', cityId: $scope.cityId, from: 1 })
+    }
+
+    $scope.gotoWriteReviews = function() {
+        $state.go('write-review');
+    }
+
+    
 
 });
