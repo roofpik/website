5badc8c8fd878079383fa788013d3437c8176264
anuporwal <anu@roofpik.com>
2017-01-30 16:29:32 +0530

    corrected navigation

--- js/navigator.js
+++ js/navigator.js
@@ -1,4 +1,10 @@
 var t = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+var url = window.location.href;
+var res = url.split("#");
 if(t){
-    window.location.href = 'http://mobile.roofpik.com';
+	if(res[1]){
+    	window.location.href = 'http://mobile.roofpik.com/#'+res[1];	
+	} else {
+		window.location.href = 'http://mobile.roofpik.com';	
+	}
 }
