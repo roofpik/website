1c01b0ff6b62a15ad831a5f1b9b923eba3c494fd
Arpit Mittal <arpit@roofpik.com>
2017-02-04 15:08:50 +0530

    template created

--- js/controllers/about-us.ctrl.js
+++
@@ -1,8 +0,0 @@
-app.controller('aboutUsCtrl', function($scope, $timeout, UserTokenService, $location){
-    var timestamp = new Date().getTime();
-    var urlInfo = {
-        url: $location.path()
-    }
-    UserTokenService.checkToken(urlInfo, timestamp, 1);
-    loading(false);
-});
---
+++ js/controllers/home/home.ctrl.js
@@ -0,0 +1,4 @@
+app.controller('homeCtrl',['$scope', function($scope){
+
+
+}]);
