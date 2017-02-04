60d14decf81540a2c9bd4c4837d755d17483023c
Digamber-Sharma <dgsharmad@gmail.com>
2017-02-04 18:17:06 +0530

    home page search

--- js/controllers/home-ctrl.js
+++ js/controllers/home-ctrl.js
@@ -1,4 +1,31 @@
 app.controller('home-ctrl', function($scope){
-	    $('input#input_text').characterCounter();
+
+	$('#location').click(function() {
+		console.log("called");
+	});
+	    $('input.autocompletefind').autocomplete({
+	    	limit: 10,
+         data: {
+             "Apple": null,
+             "App": null,
+             "Ap": '',
+             "Microsoft": 'http://placehold.it/250x250'
+         }
+         
+     });
+
+$('input.autocompletenear').autocomplete({
+	    	getData: function (value, callback) {
+
+	    	};
+         data:  {
+             "Apple Near": null,
+             "App": null,
+             "Ap": '',
+             "Microsoft": 'http://placehold.it/250x250'
+             
+         }
+    
+     });
 
 })
