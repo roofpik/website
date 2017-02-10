573c074be0f804df31520aa5f1e5b5d6c847756e
pratyusharya <pratyush@roofpik.com>
2017-02-10 15:09:05 +0530

    home page updated

--- js/directives/directives.js
+++ js/directives/directives.js
@@ -18,3 +18,13 @@ app.directive('gallery', function() {
         }
     }
 });
+
+app.directive('header', function() {
+    return {
+        restrict: 'A', //This menas that it will be used as an attribute and NOT as an element. I don't like creating custom HTML elements
+        replace: true,
+        scope: { user: '=' }, // This is one of the cool things :). Will be explained in post.
+        templateUrl: "/js/directives/header.html",
+        controller: 'headerCtrl'
+    }
+});
