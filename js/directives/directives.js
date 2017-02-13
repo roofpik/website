e6d163b53b8fbd8f06b9bfbcf70b628343f92390
Digamber-Sharma <dgsharmad@gmail.com>
2017-02-13 19:30:04 +0530

    home page roofpik

--- js/directives/directives.js
+++ js/directives/directives.js
@@ -29,6 +29,16 @@ app.directive('header', function() {
     }
 });
 
+app.directive('footer', function() {
+    return {
+        restrict: 'A', //This menas that it will be used as an attribute and NOT as an element. I don't like creating custom HTML elements
+        replace: true,
+        scope: { user: '=' }, // This is one of the cool things :). Will be explained in post.
+        templateUrl: "/js/directives/footer.html",
+        controller: 'headerCtrl'
+    }
+});
+
 
 app.directive('login', function() {
     return {
