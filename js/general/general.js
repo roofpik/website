4c331380eb4c70d66b1aa039706c5670d68a9977
Digamber-Sharma <dgsharmad@gmail.com>
2017-02-14 13:01:55 +0530

    home search complete

--- js/general/general.js
+++ js/general/general.js
@@ -36,3 +36,89 @@ function deleteLocalStorage(name){
 function getLocalStorage(name){
     return JSON.parse(localStorage.getItem(name));
 }
+
+// Search variable
+
+var searchObject = {
+   'residential': [
+       {
+           icon: 'images/homeicon/CGHS.png',
+           name: 'CGHS',
+           id: 1
+       },
+       {
+           icon: 'images/homeicon/Apartment.png',
+           name: 'Apartment',
+           id: 2
+       },
+       {
+           icon: 'images/homeicon/Villa-penthouse.png',
+           name: 'Penthouse / Villas',
+           id: 3
+       },
+       {
+           icon: 'images/homeicon/Lowrise.png',
+           name: 'Low Rise / Independent Floors',
+           id: 4
+       },
+       {
+           icon: 'images/homeicon/Commercial.png',
+           name: 'Explore Locality',
+           id: 5
+       }
+   ],
+   'commercial':[
+       {
+           icon: 'images/homeicon/Malls.png',
+           name: 'Malls',
+           id: 1
+       },
+       {
+           icon: 'images/homeicon/OpenMarkets.png',
+           name: 'Open Markets',
+           id: 2
+       },
+       {
+           icon: 'images/homeicon/Business Center.png',
+           name: 'Business Centers',
+           id: 3
+       },
+       {
+           icon: 'images/homeicon/ITParks.png',
+           name: 'IT Parks',
+           id: 3
+       },
+       {
+           icon: 'images/homeicon/Commercial.png',
+           name: 'Co-work',
+           id: 3
+       },
+       {
+           icon: 'images/homeicon/Commercial.png',
+           name: 'Explore Locality',
+           id: 6
+       }
+   ],
+   'pg': [
+       {
+           icon: 'images/homeicon/Commercial.png',
+           name: 'Girls Hostels',
+           id: 1
+       },
+       {
+           icon: 'images/homeicon/Commercial.png',
+           name: 'Boys Hostels',
+           id: 2
+       },
+       {
+           icon: 'images/homeicon/Commercial.png',
+           name: 'Co-host',
+           id: 3
+       },
+       {
+           icon: 'images/homeicon/Commercial.png',
+           name: 'Explore Locality',
+           id: 4
+       }
+   ]
+};
