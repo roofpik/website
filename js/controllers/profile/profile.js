6087c74d2d96838b66b3c134ed6e9920a0c3a04e
pratyusharya <pratyush@roofpik.com>
2017-02-20 18:39:54 +0530

    Profile Management Page

--- js/controllers/profile/profile.js
+++ js/controllers/profile/profile.js
@@ -1,14 +1,18 @@
-app.controller('profileCtrl', function($scope) {
+app.controller('profileCtrl', ['$scope', function($scope) {
     document.title = "My Profile";
 
     $scope.userId = '2GajtmSmqWaz6iLxOMyRV7xqGhy1';
     $scope.user = {};
+    $scope.image = "http://icons.iconseeker.com/png/fullsize/crystal-clear-actions/thumbnail.png"
+    $scope.newPassword = '';
+    $scope.newPasswordVerification = '';
     $scope.user.userId = $scope.userId;
     $scope.disableFirstName = true;
+    $scope.disablePwd = true;
+    $scope.disableRePwd = true;
     $scope.disableLastName = true;
     $scope.disablePhoneNumber = true;
     $scope.disableAddress = true;
-    $scope.disableCity = true;
     console.log('called');
     getUserData();
 
@@ -22,12 +26,17 @@ app.controller('profileCtrl', function($scope) {
             $scope.user.city = snapshot.val().address.cityName;
             $scope.user.emailId = snapshot.val().email.emailAddress;
             $scope.user.phoneNumber = snapshot.val().mobile.mobileNum;
-            $scope.password = $scope.user.password;
         });
     }
 
     $scope.enableFirstName = function() {
         $scope.disableFirstName = false;
+    }
+    $scope.enablePwd = function() {
+        $scope.disablePwd = false;
+    }
+    $scope.enableRePwd = function() {
+        $scope.disableRePwd = false;
     }
     $scope.enableLastName = function() {
         $scope.disableLastName = false;
@@ -38,28 +47,49 @@ app.controller('profileCtrl', function($scope) {
     $scope.enableAddress = function() {
         $scope.disableAddress = false;
     }
-    $scope.enableCity = function() {
-        $scope.disableCity = false;
-    }
     $scope.blurFirstName = function() {
         console.log($scope.user.firstName)
-        db.ref('users/' + $scope.userId + '/' + 'fname').set($scope.user.firstName);
         $scope.disableFirstName = true;
+        db.ref('users/' + $scope.userId + '/' + 'fname').set($scope.user.firstName);
     }
     $scope.blurLastName = function() {
-        db.ref('users/' + $scope.userId + '/' + 'lname').set($scope.user.lastName);
         $scope.disableLastName = true;
+        db.ref('users/' + $scope.userId + '/' + 'lname').set($scope.user.lastName);
     }
     $scope.blurPhoneNumber = function() {
-        db.ref('users/' + $scope.userId + '/' + 'mobile/mobileNum').set($scope.user.lastName);
         $scope.disablePhoneNumber = true;
+        db.ref('users/' + $scope.userId + '/' + 'mobile/mobileNum').set($scope.user.mobileNum);
     }
     $scope.blurAddress = function() {
-        db.ref('users/' + $scope.userId + '/' + 'address/addressLine1').set($scope.user.lastName);
         $scope.disableAddress = true;
+        db.ref('users/' + $scope.userId + '/' + 'address/addressLine1').set($scope.user.address);
     }
-    $scope.blurCity = function() {
-        db.ref('users/' + $scope.userId + '/' + 'address/city').set($scope.user.lastName);
-        $scope.disableCity = true;
+    $scope.blurPwd = function() {
+        $scope.disablePwd = true;
+        if ($scope.newPassword == $scope.password) {
+            swal("New Password Can't Be The Same As The Old Password", "Try Again");
+        }
+        if ($scope.newPassword.length < 8) {
+            swal("The Password Must Be Atleast 8 Characters Long", "Try Again");
+        }
     }
-})
+    $scope.blurRePwd = function() {
+        $scope.disableRePwd = true;
+        if ($scope.newPassword != $scope.newPasswordVerification) {
+            swal("Passwords Don't Match", "Try Again");
+        } else {
+            db.ref('users/' + $scope.userId + '/' + 'tempPassword').set($scope.newPassword)
+            swal("Password Successfully Changed", "Congratulations!", "success")
+        }
+    }
+
+    $scope.getFileDetails = function(file) {
+        var file = file.files[0];
+        var image = "https://getuikit.com/v2/docs/images/" + file.name;
+        changeImage(image);
+    }
+
+    function changeImage(image) {
+        $scope.image = image;
+    }
+}])
