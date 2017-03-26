b0543dc792bfb7268719c74b70e2675ca09f64c7
pratyusharya <pratyush@roofpik.com>
2017-03-26 13:31:40 +0530

    basic search completed

--- js/controllers/home.ctrl.js
+++ js/controllers/home.ctrl.js
@@ -54,7 +54,7 @@ app.controller('searchCtrl', function($scope, $timeout, $http, $state) {
         closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
         draggable: true // Choose whether you can drag to open on touch screens
     });
-    $('select').material_select();
+    // $('select').material_select();
     $('.slider').slider();
     $('.dropdown-button').dropdown();
     $('.carousel').carousel();
@@ -114,7 +114,11 @@ app.controller('searchCtrl', function($scope, $timeout, $http, $state) {
     $scope.showBasicSearch = false;
     $scope.cityId = '-KYJONgh0P98xoyPPYm9';
     $scope.filters = {};
-
+    $scope.showMinPrice = false;
+    $scope.showMaxPrice = false;
+    $scope.minPrice = 0;
+    $scope.maxPrice = 50000;
+    $scope.priceList = [10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000, 70000, 75000, 80000, 85000, 90000, 95000, 100000, 105000, 110000, 115000, 120000, 125000, 130000, 135000, 140000, 145000, 150000];
     $("#type-selection").focusin(function() {
         $timeout(function() {
             $scope.showSearch = true;
@@ -126,7 +130,7 @@ app.controller('searchCtrl', function($scope, $timeout, $http, $state) {
     $("#type-selection").focusout(function() {
         $timeout(function() {
             $scope.showSearch = false;
-        }, 500);
+        }, 1000);
     });
     $("#location-selection").focusin(function() {
         $timeout(function() {
@@ -139,8 +143,32 @@ app.controller('searchCtrl', function($scope, $timeout, $http, $state) {
     $("#location-selection").focusout(function() {
         $timeout(function() {
             $scope.showSearch1 = false;
-        }, 500);
+        }, 1000);
     });
+
+    $('#min-price').focusin(function() {
+        $timeout(function() {
+            $scope.showMinPrice = true;
+        }, 100);
+    })
+
+    $('#min-price').focusout(function() {
+        $timeout(function() {
+            $scope.showMinPrice = false;
+        }, 2000);
+    })
+
+    $('#max-price').focusin(function() {
+        $timeout(function() {
+            $scope.showMaxPrice = true;
+        }, 100);
+    })
+
+    $('#max-price').focusin(function() {
+        $timeout(function() {
+            $scope.showMaxPrice = false;
+        }, 2000);
+    })
 
     db.ref('locations/country/-K_43TEI8cBodNbwlKqJ/micromarket/city/-KYJONgh0P98xoyPPYm9/places').once('value', function(snapshot) {
         $timeout(function() {
@@ -173,14 +201,31 @@ app.controller('searchCtrl', function($scope, $timeout, $http, $state) {
         })
     }
 
-    $scope.selectLocality = function(id){
-        for(key in $scope.micromarkets){
-            if(key == id){
+    $scope.selectLocality = function(id) {
+        for (key in $scope.micromarkets) {
+            if (key == id) {
                 $scope.micromarkets[key].selected = true;
             } else {
                 $scope.micromarkets[key].selected = false;
             }
         }
+    }
+
+    $scope.priceChanging = function(type) {
+        if (type == 'min') {
+            if ($scope.filters.rent.min) {
+                $scope.minPrice = $scope.filters.rent.min;
+            } else {
+                $scope.minPrice = 0;
+            }
+        } else {
+            if ($scope.filters.rent.max) {
+                $scope.maxPrice = $scope.filters.rent.max;
+            } else {
+                $scope.maxPrice = 50000;
+            }
+        }
+        console.log($scope.minPrice);
     }
 
     // get search results based on the string in input box
@@ -241,23 +286,136 @@ app.controller('searchCtrl', function($scope, $timeout, $http, $state) {
         }
     }
 
-    $scope.search = function(){
-        if($scope.showBasicSearch){
+    $scope.search = function() {
+        if ($scope.showBasicSearch) {
             console.log($scope.filters);
+            var parameters = {};
+            if ($scope.filters.micro) {
+                for (key in $scope.filters.micro) {
+                    if ($scope.filters.micro[key]) {
+                        if (parameters.micro) {
+                            parameters.micro += ',';
+                        } else {
+                            parameters.micro = '';
+                        }
+                        parameters.micro += key;
+                    }
+                }
+            }
+            if ($scope.filters.locations) {
+                for (key in $scope.filters.locations) {
+                    if ($scope.filters.micro) {
+                        if ($scope.filters.micro[key]) {
+                            console.log('do nothing');
+                        } else {
+                            for (key1 in $scope.filters.locations[key]) {
+                                if ($scope.filters.locations[key][key1]) {
+                                    if (parameters.loc) {
+                                        parameters.loc += ',';
+                                    } else {
+                                        parameters.loc = '';
+                                    }
+                                    parameters.loc += key1;
+                                }
+                            }
+                        }
+                    } else {
+                        for (key1 in $scope.filters.locations[key]) {
+                            if ($scope.filters.locations[key][key1]) {
+                                if (parameters.loc) {
+                                    parameters.loc += ',';
+                                } else {
+                                    parameters.loc = '';
+                                }
+                                parameters.loc += key1;
+                            }
+                        }
+                    }
+                }
+            }
+            if ($scope.filters.bhk) {
+                for (key in $scope.filters.bhk) {
+                    if ($scope.filters.bhk[key]) {
+                        if (parameters.bhk) {
+                            parameters.bhk += ',';
+                        } else {
+                            parameters.bhk = '';
+                        }
+                        parameters.bhk += key;
+                    }
+                }
+            }
+            if ($scope.filters.rent) {
+                if ($scope.filters.rent.min && $scope.filters.rent.max) {
+                    parameters.rent = $scope.filters.rent.min + ',' + $scope.filters.rent.max;
+                } else if ($scope.filters.rent.min) {
+                    parameters.rent = $scope.filters.rent.min + ',' + 150000;
+                } else if ($scope.filters.rent.max) {
+                    parameters.rent = 0 + ',' + $scope.filters.rent.max;
+                } else {
+                    parameters.rent = 0 + ',' + 150000;
+                }
+            }
+            if ($scope.filters.ptype) {
+                parameters.ptype = $scope.filters.ptype;
+            }
+            $state.go('listing', { micro: parameters.micro, loc: parameters.loc, ptype: parameters.ptype, bhk: parameters.bhk, rent: parameters.rent });
         }
     }
 
-    $scope.selectBhk = function(bhk){
+    $scope.selectAllLocality = function(id) {
+        if ($scope.filters.micro[id]) {
+            for (key in $scope.micromarkets) {
+                if (key == id) {
+                    for (key1 in $scope.micromarkets[key].localities) {
+                        if (!$scope.filters.locations) {
+                            $scope.filters.locations = {};
+                        }
+                        if (!$scope.filters.locations[key]) {
+                            $scope.filters.locations[key] = {};
+                        }
+                        $scope.filters.locations[key][key1] = true;
+                    }
+                }
+            }
+        } else {
+            delete $scope.filters.locations[id];
+            delete $scope.filters.micro[id];
+            if (Object.keys($scope.filters.micro).length == 0) {
+                delete $scope.filters.micro;
+            }
+            if (Object.keys($scope.filters.locations).length == 0) {
+                delete $scope.filters.locations;
+            }
+        }
+    }
+
+    $scope.selectBhk = function(bhk) {
         console.log(bhk);
-        if(!$scope.filters.bhk){
+        if (!$scope.filters.bhk) {
             $scope.filters.bhk = {};
         }
-        if($scope.filters.bhk[bhk]){
+        if ($scope.filters.bhk[bhk]) {
             delete $scope.filters.bhk[bhk]
         } else {
             $scope.filters.bhk[bhk] = true;
         }
         console.log($scope.filters.bhk);
+    }
+
+    $scope.selectPrice = function(amount, type) {
+        console.log(type);
+        if (!$scope.filters.rent) {
+            $scope.filters.rent = {};
+        }
+        if (type == 'min') {
+            $scope.filters.rent.min = amount;
+            $scope.minPrice = amount;
+        } else {
+            $scope.filters.rent.max = amount;
+            $scope.maxPrice = amount;
+        }
+        console.log($scope.filters);
     }
 
     $scope.searchLocations = function() {
@@ -379,6 +537,9 @@ app.controller('searchCtrl', function($scope, $timeout, $http, $state) {
         } else {
             $scope.searchTitle = 'Basic Search';
         }
+
+        $('select').material_select();
+
     }
 });
 
