ef0f95b17cdc49620d120f34c66be90a02f12809
Arpit Mittal <arpit@roofpik.com>
2017-04-14 20:25:34 +0530

    home page search

--- js/controllers/offers.js
+++ js/controllers/offers.js
@@ -1,8 +1,22 @@
 app.controller('OffersCtrl', function($scope, $state, $timeout) {
-    
-     
-  $('#offer_modal_popup').modal();
+
+
+    $('#offer_modal_popup').modal();
+
+    db.ref('/partner/city/-KYJONgh0P98xoyPPYm9/coupon').once('value', function(snapshot) {
+        $timeout(function() {
+
+            $scope.allCoupon = snapshot.val();
+            console.log($scope.allCoupon);
+            // for (key in allCoupon) {
+            //     for (img in allCoupon[key]) {
+            //         db.ref('images/' + img).once('value', function(snapshot) {
+                    	
+            //         });
+            //     }
+            // }
+        }, 200)
+    });
 
 
 });
-
